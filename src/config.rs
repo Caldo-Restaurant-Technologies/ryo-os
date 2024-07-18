cdcf7b158ff73db08dc419e5eb5f91b394023493
Riley <rileykhernandez@gmail.com>
2024-07-18 12:21:52 -0700

    added gantry calibration stuff to hmi

--- src/config.rs
+++ src/config.rs
@@ -35,6 +35,8 @@ pub const SEALER_RETRACT_SET_POINT: isize = 50;
 pub const TRAP_DOOR_SLOT_ID: usize = 2;
 pub const TRAP_DOOR_OPEN_OUTPUT_ID: usize = 3;
 pub const TRAP_DOOR_CLOSE_OUTPUT_ID: usize = 2;
+pub const TRAP_DOOR_EXTEND_SET_POINT: isize = 4090;
+pub const TRAP_DOOR_RETRACT_SET_POINT: isize = 50;
 
 pub const HEATER_SLOT_ID: usize = 3;
 pub const HEATER_OUTPUT_ID: usize = 2;
